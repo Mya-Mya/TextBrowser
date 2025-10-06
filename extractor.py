616b5cdcfd355f3e9305b57dc99cc9caeb435d8d
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2025-10-06 17:11:52 +0900

    Format

--- extractor.py
+++ extractor.py
@@ -40,7 +40,7 @@ def extract_content_html(url: str) -> RenderingArg:
 
     try:
         # Fetch HTML
-        headers = {'User-Agent': 'Mozilla/5.0'}
+        headers = {"User-Agent": "Mozilla/5.0"}
         response = requests.get(url, headers=headers, timeout=5)
         response.raise_for_status()
         response.encoding = "utf-8"
@@ -51,11 +51,14 @@ def extract_content_html(url: str) -> RenderingArg:
         # Extract Anchors
         for anchor_element in soup.find_all("a"):
             out.anchors.append(
-                Anchor(href=anchor_element.attrs.get("href"), content=anchor_element.get_text(strip=True))
+                Anchor(
+                    href=anchor_element.attrs.get("href"),
+                    content=anchor_element.get_text(strip=True),
+                )
             )
 
         # Eliminate seemingly noisy tags
-        for tag in soup(['script', 'style', 'header', 'footer', 'nav', 'aside']):
+        for tag in soup(["script", "style", "header", "footer", "nav", "aside"]):
             tag.decompose()
 
         # Extract Lists
@@ -63,8 +66,10 @@ def extract_content_html(url: str) -> RenderingArg:
         out.ols = [extract_li_list(ol) for ol in soup.find_all("ol")]
 
         # Extract seemingly content tags
-        candidates = soup.find_all(['article', 'main', 'section', 'div'])
-        longest = max(candidates, key=lambda tag: len(tag.get_text(strip=True)), default=None)
+        candidates = soup.find_all(["article", "main", "section", "div"])
+        longest = max(
+            candidates, key=lambda tag: len(tag.get_text(strip=True)), default=None
+        )
         assert longest is not None, "No content found."
 
         # Extract content
@@ -75,9 +80,11 @@ def extract_content_html(url: str) -> RenderingArg:
         out.has_meta = True
         out.original_html_length = len(original_html)
         out.content_html_length = len(out.content_html)
-        out.compression_rate = \
-            "?" if out.original_html_length == 0 \
-                else f"{100 * out.content_html_length / out.original_html_length:.2f}%"
+        out.compression_rate = (
+            "?"
+            if out.original_html_length == 0
+            else f"{100 * out.content_html_length / out.original_html_length:.2f}%"
+        )
 
     except Exception as e:
         out.content_html = str(e)
